25aaf4bce6812338a2ad56c24015fdc89704ad03
anwarmoazam <anwar.palimarwar@gmail.com>
2024-01-09 17:52:01 +0530

    leetcode

--- index.js
+++ index.js
@@ -151,3 +151,54 @@ function censor(str) {
 
 console.log(censor("My Name is anwar moazam"));
 
+
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+*/
+
+var maxArea = function (arr){
+    let maxHeight = {};
+    let secondMaxHeight = {};
+    let firstMax = arr[0];
+    let secondMax = -Infinity;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] >= firstMax){
+            firstMax = arr[i];
+            console.log('First Max : ',firstMax);
+            maxHeight = {}
+            maxHeight[i] = firstMax;
+        }
+    }
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > secondMax && arr[i] < firstMax){
+            secondMax = arr[i];
+            console.log('Second Max : ',secondMax);
+            secondMaxHeight = {};
+            secondMaxHeight[i] = secondMax;
+        }
+    }
+    console.log(maxHeight,secondMaxHeight);
+    return {...maxHeight,...secondMaxHeight};
+}
+
+console.log(maxArea([100,2,5,2,3,101,1,88]));
+
